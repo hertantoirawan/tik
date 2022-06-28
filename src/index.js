d0835ae4e5851d987f546149139b07323ad1d36f
Hertanto Irawan <hertanto.irawan@outlook.com>
2022-06-28 23:15:13 +0700

    make connect 2 in 3x3 grid work

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import './styles.css';
 import anime from 'animejs/lib/anime.es.js';
 
-const gridWidth = 4;
-const gridHeight = 3;
+const GRID_WIDTH = 3;
+const GRID_HEIGHT = 3;
+const NUM_CONNECTED_DOTS = 2;
 
 const grid = document.querySelector('.grid');
-const gridSize = gridWidth * gridHeight;
+const gridDots = [];
+let score = 0;
 
+// orange green blue
 const colors = ['rgb(255, 0, 0)', 'rgb(0, 128, 0)', 'rgb(0, 0, 255)'];
 
-for (let i = 0; i < gridSize; i += 1) {
-  const dot = document.createElement('div');
-  const x = i % gridWidth;
-  const y = Math.floor(i / gridWidth);
+for (let x = 0; x < GRID_WIDTH; x += 1) {
+  const tempDots = [];
 
-  dot.setAttribute('id', `dot-${x}-${y}`);
-  if (y === 0) {
+  for (let y = 0; y < GRID_HEIGHT; y += 1) {
+    const dot = document.createElement('div');
+
+    dot.setAttribute('id', `dot-${x}-${y}`);
     dot.classList.add('dot');
-    dot.style.backgroundColor = colors[0];
-  } else if (y === 1) {
-    dot.classList.add('dot');
-    dot.style.backgroundColor = colors[1];
-  } else {
-    dot.classList.add('dot');
-    dot.style.backgroundColor = colors[2];
+    const color = colors[anime.random(0, colors.length - 1)];
+    dot.style.backgroundColor = color;
+
+    tempDots.push(color);
+    grid.appendChild(dot);
   }
-  grid.appendChild(dot);
+
+  gridDots.push(tempDots);
 }
+console.log(gridDots);
 
 const setup = anime({
   targets: '.dot',
@@ -43,26 +46,64 @@ const getDotsAbove = (id) => {
   const y = idTokens[2];
 
   for (let i = 0; i < y; i += 1) {
-    dots.push(`#dot-${x}-${i}`);
+    if (gridDots[x][i] !== '') dots.push(`#dot-${x}-${i}`);
   }
 
   return dots;
 };
 
-const removeDotsAbove = (id) => {
-  const dotsAbove = getDotsAbove(id);
-  console.log(dotsAbove);
+let selectedDots = [];
 
-  const remove = anime.timeline({
-    autoplay: false,
+const getDotsAboveSelected = () => {
+  const dotsAbove = [];
+
+  selectedDots.forEach((selectedDot) => dotsAbove.push(getDotsAbove(selectedDot)));
+
+  return dotsAbove.flat();
+};
+
+const getEmptyDots = () => {
+  const emptyDots = [];
+  const emptyColumns = {};
+
+  selectedDots.forEach((selectedDot) => {
+    const x = selectedDot.split('-')[1];
+
+    if (x in emptyColumns) emptyColumns[x] += 1;
+    else emptyColumns[x] = 1;
   });
 
-  // remove dissapearing dot
-  remove.add({
-    targets: `#${id}`,
+  Object.keys(emptyColumns).forEach((key) => {
+    for (let i = 0; i < emptyColumns[key]; i += 1) {
+      emptyDots.push(`#dot-${key}-${i}`);
+    }
+  });
+
+  return emptyDots;
+};
+
+const removeConnectedDots = (remove) => {
+  selectedDots.forEach((selectedDot) => {
+    const idTokens = selectedDot.split('-');
+    gridDots[idTokens[1]][idTokens[2]] = '';
+  });
+
+  anime({
+    targets: selectedDots.map((selectedDot) => `#${selectedDot}`),
     opacity: 0,
-    autoplay: false,
+    scale: 1,
   });
+
+  // remove.add({
+  //   targets: selectedDots.map((selectedDot) => `#${selectedDot}`),
+  //   opacity: 0,
+  //   scale: 1,
+  //   autoplay: false,
+  // });
+};
+
+const dropDotsAbove = (remove) => {
+  const dotsAbove = getDotsAboveSelected();
 
   if (dotsAbove.length > 0) {
     // make dots above the dissapearing dot fall
@@ -71,52 +112,90 @@ const removeDotsAbove = (id) => {
       translateY: (dotsAbove.length === 1) ? 32 : 28,
       autoplay: false,
     });
+  }
+};
 
-    // fix dot positions after falling dots animation
-    for (let i = dotsAbove.length - 1; i >= 0; i -= 1) {
-      remove.add({
-        targets: (i === dotsAbove.length - 1) ? `#${id}` : `${dotsAbove[i + 1]}`,
-        background: document.querySelector(`${dotsAbove[i]}`).style.backgroundColor,
-        opacity: 1,
-        easing: 'steps(1)',
-        autoplay: false,
-      });
+const replaceEmptyDots = (replace) => {
+  const emptyDots = getEmptyDots();
+  console.log(`empty dots: ${emptyDots}`);
+  // fill in empty coordinates with new dots
+  replace.add({
+    targets: emptyDots,
+    background: () => colors[anime.random(0, colors.length - 1)],
+    translateY: [-20, 0],
+    opacity: 1,
+    autoplay: false,
+  });
+};
 
-      remove.add({
-        targets: dotsAbove[i + 1],
-        background: document.querySelector(`${dotsAbove[i]}`).style.backgroundColor,
-        translateY: 0,
+const animatedFixDotsPositionsColors = (fix) => {
+  for (let i = 0; i < selectedDots.length; i += 1) {
+    const dotsAbove = getDotsAbove(selectedDots[i]);
+
+    if (dotsAbove.length > 0) {
+      // fix dot colors after falling dots animation
+      for (let j = dotsAbove.length - 1; j >= 0; j -= 1) {
+        fix.add({
+          targets: (j === (dotsAbove.length - 1)) ? `#${selectedDots[i]}` : `${dotsAbove[j + 1]}`,
+          background: document.querySelector(`${dotsAbove[j]}`).style.backgroundColor,
+          opacity: 1,
+          easing: 'linear',
+          autoplay: false,
+        });
+      }
+
+      // fix dots positions after falling dots animation
+      // TODO: need to hide animation
+      fix.add({
+        targets: dotsAbove,
+        translateY: (el, index) => ((index === 0) ? el.style.transform.translateY : 0),
+        easing: 'linear',
         autoplay: false,
       });
     }
-
-    // fill in empty coordinates with new dots
-    remove.add({
-      targets: dotsAbove[0],
-      background: colors[anime.random(0, colors.length - 1)],
-      translateY: [-20, 0],
-      autoplay: false,
-    });
-  } else {
-    // fill in empy coordinates with new dots
-    remove.add({
-      targets: `#${id}`,
-      opacity: 1,
-      translateY: [-20, 0],
-      autoplay: false,
-    });
   }
-
-  remove.play();
 };
 
-let selectedDots = [];
+const fixDotsPositionsColors = (fix) => {
+  let column = GRID_WIDTH - 1;
+  while (column >= 0) {
+    let row = GRID_HEIGHT - 1;
 
-const selectDot = (id) => {
-  const select = anime.timeline({
-    autoplay: false,
-  });
+    while (row >= 0) {
+      if (gridDots[row][column] === '') {
+        for (let i = row - 1; i >= 0; i -= 1) {
+          if (gridDots[i][column] !== '') {
+            // swap empty dot with first dot above
+            const nonEmptyDot = gridDots[i][column];
+            gridDots[i][column] = '';
+            gridDots[row][column] = nonEmptyDot;
 
+            // animate dots position fix
+            anime({
+              targets: `#dot-${i}-${column}`,
+              opacity: 0,
+            });
+            anime({
+              targets: `#dot-${row}-${column}`,
+              background: nonEmptyDot,
+              opacity: 1,
+
+            });
+
+            break;
+          }
+        }
+      }
+      row -= 1;
+    }
+    column -= 1;
+  }
+
+  console.log('after');
+  console.log(gridDots);
+};
+
+const animatedSelectDot = (select, id) => {
   if (selectedDots.includes(id)) {
     select.add({
       targets: `#${id}`,
@@ -132,24 +211,40 @@ const selectDot = (id) => {
 
     selectedDots.push(id);
   }
-
-  select.play();
 };
 
-const unselectDots = () => {
+const selectDot = (select, id) => {
+  if (selectedDots.includes(id)) {
+    selectedDots.splice(selectedDots.indexOf(id), 1);
+    anime({
+      targets: `#${id}`,
+      scale: 1,
+    });
+  } else {
+    selectedDots.push(id);
+    anime({
+      targets: `#${id}`,
+      scale: 1.2,
+    });
+  }
+};
+
+const unselectDots = (unselect) => {
   selectedDots = [];
 
-  const unselect = anime.timeline({
-    autoplay: false,
-  });
-
-  unselect.add({
+  anime.timeline({
+    autoplay: true,
+  }).add({
     targets: '.dot',
     scale: 1,
-    autoplay: false,
+    delay: 1000,
   });
 
-  unselect.play();
+  // unselect.add({
+  //   targets: '.dot',
+  //   scale: 1,
+  //   autoplay: false,
+  // });
 };
 
 const isValidMove = () => {
@@ -173,7 +268,7 @@ const isValidMove = () => {
 };
 
 const dotsConnected = () => {
-  if (selectedDots.length < 2) return false;
+  if (selectedDots.length < NUM_CONNECTED_DOTS) return false;
   if (!isValidMove()) return false;
 
   let color;
@@ -182,40 +277,40 @@ const dotsConnected = () => {
 
     if (i === 0) color = thisDotColor;
     else if (color !== thisDotColor) {
-      unselectDots();
       return false;
     }
   }
 
+  score += (selectedDots.length - NUM_CONNECTED_DOTS) + 1;
   return true;
 };
 
 const dots = document.querySelectorAll('.dot');
 dots.forEach((dot) => {
   dot.onclick = () => {
-    console.log(dot.id);
-    console.log(`before: ${selectedDots}`);
-    selectDot(dot.id);
-    console.log(`after: ${selectedDots}`);
-    if (selectedDots.length >= 2) {
+    const timeline = anime.timeline({
+      autoplay: false,
+    });
+
+    selectDot(timeline, dot.id);
+
+    if (selectedDots.length >= NUM_CONNECTED_DOTS) {
       if (dotsConnected()) {
-        console.log('dots connected');
-        selectedDots.forEach((selectedDot) => removeDotsAbove(selectedDot));
+        console.log(`dots connected: ${selectedDots}`);
+        removeConnectedDots(timeline);
+        fixDotsPositionsColors(timeline);
+        // replaceEmptyDots(timeline);
       }
-      unselectDots();
+      unselectDots(timeline);
     }
+
+    console.log(`score: ${score}`);
+    // timeline.play();
   };
 });
 
-// // document.querySelector('#oneone').onclick = replace;
-// const rect = document.querySelector('#dot-0-1').getBoundingClientRect();
-// console.log(rect.top, rect.right, rect.bottom, rect.left);
-
-// const dot = document.querySelector('#dot-3-1').getBoundingClientRect();
-// console.log(dot.top, dot.right, dot.bottom, dot.left);
-
-const getCenterCoordinates = (element) => {
-  const shape = element.getBoundingClientRect();
-  return { x: shape.left + (shape.width / 2), y: (shape.top + (shape.height / 2)) };
-};
-console.log(getCenterCoordinates(document.querySelector('#dot-3-1')));
+// const getCenterCoordinates = (element) => {
+//   const shape = element.getBoundingClientRect();
+//   return { x: shape.left + (shape.width / 2), y: (shape.top + (shape.height / 2)) };
+// };
+// console.log(getCenterCoordinates(document.querySelector('#dot-3-1')));
